2dc54cf676d4c31bc3cc747ab19a18b3334dda0a
JiJetu <jijetu2@gmail.com>
2024-01-20 20:33:33 +0600

    added delete and update button and delete user successfully

--- server/src/components/Users.jsx
+++ server/src/components/Users.jsx
@@ -1,14 +1,28 @@
-import { useLoaderData } from "react-router-dom";
+import { Link, useLoaderData } from "react-router-dom";
 
 const Users = () => {
     const users = useLoaderData();
-    console.log(users);
+
+    const handleDelete = _id => {
+        console.log(_id);
+        
+        fetch(`http://localhost:5000/users/${_id}`,{
+            method: 'DELETE'
+        })
+        .then(res => res.json())
+        .then(data => {
+            if(data.deletedCount>0){
+                alert("user deleted successfully")
+            }
+        })
+    }
     return (
         <div>
             <h1>Number of User {users.length}</h1>
             {
-                users.map(user => <p key={user._id}>{user.name} : {user.email}</p>)
+                users.map(user => <p key={user._id}>{user.name} : {user.email} <button>Update</button> <button onClick={() => handleDelete(user._id)}>X</button></p>)
             }
+            <Link to='/'><button>add user</button></Link>
         </div>
     );
 };
