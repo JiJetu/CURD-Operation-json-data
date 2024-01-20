26dde40274f800fa67dd3fffeb3cbcedf3c66747
JiJetu <jijetu2@gmail.com>
2024-01-20 19:36:01 +0600

    created a form where user can create and find the user informaition in client side

--- server/src/App.jsx
+++ server/src/App.jsx
@@ -1,33 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    const form = e.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const user ={name, email}
+    console.log(user);
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>CURD</h1>
+      <h3>Create User</h3>
+      <form onSubmit={handleSubmit}>
+        <input type="text" name='name' />
+        <br />
+        <input type="email" name="email" id="" />
+        <br />
+        <input type="submit" value="Submit" />
+      </form>
     </>
   )
 }
