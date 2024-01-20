6ea78610a77006f142372561f1f1f423419f2654
JiJetu <jijetu2@gmail.com>
2024-01-20 21:30:25 +0600

    updated user name at updateuser routh

--- server/src/components/Users.jsx
+++ server/src/components/Users.jsx
@@ -1,26 +1,34 @@
+import { useState } from "react";
 import { Link, useLoaderData } from "react-router-dom";
 
 const Users = () => {
-    const users = useLoaderData();
+    const loadedUsers = useLoaderData();
+    const [users, setUsers] = useState(loadedUsers)
 
     const handleDelete = _id => {
         console.log(_id);
-        
-        fetch(`http://localhost:5000/users/${_id}`,{
+
+        fetch(`http://localhost:5000/users/${_id}`, {
             method: 'DELETE'
         })
-        .then(res => res.json())
-        .then(data => {
-            if(data.deletedCount>0){
-                alert("user deleted successfully")
-            }
-        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.deletedCount > 0) {
+                    alert("user deleted successfully")
+                    const remainingUser = users.filter(user => user._id !== _id)
+                    setUsers(remainingUser)
+                }
+            })
     }
     return (
         <div>
             <h1>Number of User {users.length}</h1>
             {
-                users.map(user => <p key={user._id}>{user.name} : {user.email} <button>Update</button> <button onClick={() => handleDelete(user._id)}>X</button></p>)
+                users.map(user => <p key={user._id}>{user.name} : {user.email}
+                    <button>
+                        <Link to={`/users/${user._id}`}>Update</Link>
+                    </button>
+                    <button onClick={() => handleDelete(user._id)}>X</button></p>)
             }
             <Link to='/'><button>add user</button></Link>
         </div>
--- server/src/main.jsx
+++ server/src/main.jsx
@@ -8,6 +8,7 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import Users from './components/Users.jsx';
+import UpdateUser from './components/UpdateUser.jsx';
 
 const router = createBrowserRouter([
   {
@@ -18,6 +19,11 @@ const router = createBrowserRouter([
     path:'/users',
     element: <Users></Users>,
     loader: () => fetch('http://localhost:5000/users')
+  },
+  {
+    path:'/users/:id',
+    element:<UpdateUser></UpdateUser>,
+    loader: ({params}) => fetch(`http://localhost:5000/users/${params.id}`)
   }
 ]);
 
